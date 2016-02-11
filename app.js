458e7988657ed6b566df45699da16d025dd0f41a
GR <484849@CTS.COM>
2016-02-11 18:53:21 +0530

    Added new'

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res) {
+    res.send("Hekko world");
+})
+app.get('/index.html', function(req, res) {
+     res.sendFile( __dirname + "/" + "index.html" );
+})
+var server =app.listen('3000', function() {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+})
