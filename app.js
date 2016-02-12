c6588bb6f221d40e9aedaa27003eebe557771cec
GR <484849@CTS.COM>
2016-02-12 10:12:41 +0530

    Changes'

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function(req, res) {
-    res.send("Hekko world");
+    res.sendFile( __dirname + "/" + "index.html" );
 })
 app.get('/index.html', function(req, res) {
      res.sendFile( __dirname + "/" + "index.html" );
 })
-var server =app.listen('3000', function() {
+var server =app.listen(process.env.PORT, function() {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
